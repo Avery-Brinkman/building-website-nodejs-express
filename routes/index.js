0498645000b8d70b20e18c79dc60b7af98f21b56
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-29 16:48:11 -0400

    Looping through lists in templates

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,18 @@ const router = express.Router();
 
 // Returns this function
 module.exports = (params) => {
+    const { speakersService } = params;
+
     // We need a route to open in the browser.
     // app.get("this is for the slash route, so that's the index route", callback)
-    router.get('/', (request, response) => {
+    router.get('/', async (request, response) => {
+        const topSpeakers = await speakersService.getList();
+
         // When the app gets a request, we will send a file to response (response.sendFile)
         // The file we want to send is located in CurrentDirectory/static/index.html
         // __dirname is CurrentDirectory
         // response.sendFile(path.join(__dirname, './static/index.html'));
-        response.render('layout', { pageTitle: 'Welcome', template: 'index' });
+        response.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers });
     });
 
     // For everything matching the /speakers route, use speakersRoute
