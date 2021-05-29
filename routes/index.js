1cb6075025b0ab8ad81af6be8a40ff8ae9a54ef1
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-29 16:50:58 -0400

    Formatting

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,15 @@ const router = express.Router();
 module.exports = (params) => {
     const { speakersService } = params;
 
-    // We need a route to open in the browser.
-    // app.get("this is for the slash route, so that's the index route", callback)
+    /* We need a route to open in the browser.
+     * app.get("this is for the slash route, so that's the index route", callback) */
     router.get('/', async (request, response) => {
         const topSpeakers = await speakersService.getList();
 
-        // When the app gets a request, we will send a file to response (response.sendFile)
-        // The file we want to send is located in CurrentDirectory/static/index.html
-        // __dirname is CurrentDirectory
-        // response.sendFile(path.join(__dirname, './static/index.html'));
+        /* When the app gets a request, we will send a file to response (response.sendFile)
+         * The file we want to send is located in CurrentDirectory/static/index.html
+         * __dirname is CurrentDirectory
+         * response.sendFile(path.join(__dirname, './static/index.html')); */
         response.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers });
     });
 
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// const { request, response } = require('express');
 const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
@@ -14,9 +13,8 @@ const routes = require('./routes');
 // express needs parenthesis bc it's something that's running
 const app = express();
 
-// This is the port that the application will be listening to. Usually, web
-// servers are on port 80, but it's convention that Express servers are on
-// 3000.
+// This is the port that the application will be listening to. Usually, web servers are on port 80,
+// but it's convention that Express servers are on 3000.
 const port = 3000;
 
 // Makes express trust cookies that are passed through a reverse proxy
@@ -35,9 +33,8 @@ app.set('views', path.join(__dirname, './views'));
 // Sets variable siteName on creation
 app.locals.siteName = 'ROUX Meetups';
 
-// This is called 'middleware'
-// It tells express to look in the static folder for each static request it recieves,
-// and if it finds a match it will send it.
+// This is called 'middleware'. It tells express to look in the static folder for each static
+// request it recieves, and if it finds a match it will send it.
 app.use(express.static(path.join(__dirname, './static')));
 
 app.use(async (request, response, next) => {
@@ -66,6 +63,7 @@ app.get('/speakers', (request, response) => {
 // We need to start the server and tell it where to listen
 app.listen(port, () => {
     // Called as soon as the port is listening
+
     // eslint-disable-next-line no-console
     console.log(`Express server listening on port ${port}.`);
 });
