40d9e08b683ff2a8637ebee040a7bef0833ccb28
Avery Brinkman <brinkmae@mail.uc.edu>
2021-05-31 15:49:20 -0400

    Validating and sanitizing user input

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 module.exports = (params) => {
@@ -8,24 +10,60 @@ module.exports = (params) => {
     router.get('/', async (request, response, next) => {
         try {
             const feedback = await feedbackService.getList();
+
+            // const errors = request.session.feedback.errors if request.session.feedback exists, else = false
+            const errors = request.session.feedback ? request.session.feedback.errors : false;
+            // After storing the errors, reset the state
+            request.session.feedback = {};
+
             return response.render('layout', {
                 pageTitle: 'Feedback',
                 template: 'feedback',
                 feedback,
+                errors,
             });
         } catch (err) {
             return next(err);
         }
     });
 
-    router.post('/', (request, response, next) => {
-        console.log(request.body);
-        try {
+    router.post(
+        '/',
+        [
+            check('name') // Check name
+                .trim() // Trims whitespace
+                .isLength({ min: 3 }) // Ensures name is at least 3 letters
+                .escape() // Escaping to make sure there is no HTML and JavaScript embedded in entry
+                .withMessage('A name is required'), // Message returned if something is wrong
+            check('email')
+                .trim()
+                .isEmail() // Checks if email is real
+                .normalizeEmail() // Formats email
+                .escape()
+                .withMessage('A valid email address is required'),
+            check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
+            check('message')
+                .trim()
+                .isLength({ min: 5 })
+                .escape()
+                .withMessage('A message is required'),
+        ],
+        (request, response, next) => {
+            const errors = validationResult(request);
+
+            if (!errors.isEmpty()) {
+                // Stores to variable called feedback on session object
+                request.session.feedback = {
+                    errors: errors.array(),
+                };
+                return response.redirect('/feedback');
+            }
+
+            console.log(request.body);
+
             return response.send('Feedback form posted');
-        } catch (err) {
-            return next(err);
         }
-    });
+    );
 
     return router;
 };
